179daa16ce71c421570536fe129bc61dd67cdd5c
Masayuki Kawasaki <nu11p0er@gmail.com>
2024-01-24 20:37:12 +0900

    Implement "Evaluating literals"

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,4 +1,8 @@
 package com.craftinginterpreters.lox;
 
 class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
 }
