98ee25074f5b44c09db542ce56565580e5996254
Masayuki Kawasaki <nu11p0er@gmail.com>
2024-02-06 22:41:47 +0900

    Implement "A Resolver Class"

---
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -0,0 +1,9 @@
+package com.craftinginterpreters.lox;
+
+class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
+    private final Interpreter interpreter;
+
+    Resolver(Interpreter interpreter) {
+        this.interpreter = interpreter;
+    }
+}
