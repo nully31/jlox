83ceedd10050eb5d38c6c8adf38fc6772e7d2e0a
Masayuki Kawasaki <nu11p0er@gmail.com>
2024-02-07 21:52:46 +0900

    Implement "Class Declarations"

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -32,8 +32,9 @@ class Parser {
     }
 
     private Stmt declaration() {
-        // declaration -> funDecl | varDecl | statement ;
+        // declaration -> classDecl | funDecl | varDecl | statement ;
         try {
+            if (match(CLASS)) return classDeclaration();
             // funDecl -> "fun" function ;
             if (match(FUN)) return function("function");
             if (match(VAR)) return varDeclaration();
@@ -43,6 +44,21 @@ class Parser {
             synchronize();
             return null;
         }
+    }
+
+    private Stmt classDeclaration() {
+        // classDecl -> "class" IDENTIFIER "{" function* "}" ;
+        Token name = consume(IDENTIFIER, "Exect class name.");
+        consume(LEFT_BRACE, "Expect '{' before class body.");
+
+        List<Stmt.Function> methods = new ArrayList<>();
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            methods.add(function("method"));
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after class body.");
+
+        return new Stmt.Class(name, methods);
     }
 
     private Stmt statement() {
